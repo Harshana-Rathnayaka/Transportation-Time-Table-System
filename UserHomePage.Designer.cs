fe160d0500546802235278cad11279918d724a52
Harshana ratnayaka <huratnayaka@gmail.com>
2021-04-30 21:59:59 +0530

    searching by the user done

--- UserHomePage.Designer.cs
+++ UserHomePage.Designer.cs
@@ -28,11 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.RefreshBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SearchLabel = new System.Windows.Forms.Label();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.SearchTypeComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BackButton
+            // 
+            this.BackButton.BackColor = System.Drawing.Color.Red;
+            this.BackButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.BackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BackButton.ForeColor = System.Drawing.Color.White;
+            this.BackButton.Location = new System.Drawing.Point(199, 339);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(141, 27);
+            this.BackButton.TabIndex = 17;
+            this.BackButton.Text = "Exit";
+            this.BackButton.UseVisualStyleBackColor = false;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // RefreshBtn
+            // 
+            this.RefreshBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.RefreshBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.RefreshBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RefreshBtn.ForeColor = System.Drawing.Color.White;
+            this.RefreshBtn.Location = new System.Drawing.Point(381, 339);
+            this.RefreshBtn.Name = "RefreshBtn";
+            this.RefreshBtn.Size = new System.Drawing.Size(141, 27);
+            this.RefreshBtn.TabIndex = 16;
+            this.RefreshBtn.Text = "Refresh";
+            this.RefreshBtn.UseVisualStyleBackColor = false;
+            this.RefreshBtn.Click += new System.EventHandler(this.RefreshBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 94);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(689, 218);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // SearchLabel
+            // 
+            this.SearchLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.SearchLabel.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchLabel.Location = new System.Drawing.Point(12, 38);
+            this.SearchLabel.Name = "SearchLabel";
+            this.SearchLabel.Size = new System.Drawing.Size(160, 23);
+            this.SearchLabel.TabIndex = 70;
+            this.SearchLabel.Text = "Search";
+            this.SearchLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Enabled = false;
+            this.SearchBox.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchBox.Location = new System.Drawing.Point(178, 64);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(177, 25);
+            this.SearchBox.TabIndex = 69;
+            this.SearchBox.TextChanged += new System.EventHandler(this.SearchBox_TextChanged);
+            // 
+            // SearchTypeComboBox
+            // 
+            this.SearchTypeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SearchTypeComboBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.SearchTypeComboBox.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchTypeComboBox.FormattingEnabled = true;
+            this.SearchTypeComboBox.Items.AddRange(new object[] {
+            "Route",
+            "Destination",
+            "Starting Point",
+            "Departure Time",
+            "Type"});
+            this.SearchTypeComboBox.Location = new System.Drawing.Point(12, 64);
+            this.SearchTypeComboBox.Name = "SearchTypeComboBox";
+            this.SearchTypeComboBox.Size = new System.Drawing.Size(160, 25);
+            this.SearchTypeComboBox.TabIndex = 71;
+            this.SearchTypeComboBox.SelectedValueChanged += new System.EventHandler(this.SearchTypeComboBox_SelectedValueChanged);
+            // 
+            // UserHomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "UserHomePage";
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(713, 382);
+            this.Controls.Add(this.SearchTypeComboBox);
+            this.Controls.Add(this.SearchLabel);
+            this.Controls.Add(this.SearchBox);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.RefreshBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.MaximizeBox = false;
+            this.Name = "UserHomePage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Available Schedules";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button RefreshBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label SearchLabel;
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.ComboBox SearchTypeComboBox;
     }
 }
--- UserHomePage.cs
+++ UserHomePage.cs
@@ -7,14 +7,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SAD_Assignment
 {
     public partial class UserHomePage : Form
     {
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-M2P60SB\SQLEXPRESS;Initial Catalog=sad_project;Integrated Security=True");
+
+        SqlDataAdapter adpt;
+        DataTable dt;
+
+        public static string searchingColumn;
+
         public UserHomePage()
         {
             InitializeComponent();
+            showData();
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+        }
+
+        private void RefreshBtn_Click(object sender, EventArgs e)
+        {
+            refreshData();
+            SearchBox.Clear();
+            SearchTypeComboBox.SelectedIndex = -1;
+            SearchBox.Enabled = false;
+        }
+
+
+        private void refreshData()
+        {
+            // refreshing the data 
+            try
+            {
+                con.Open();
+
+                adpt = new SqlDataAdapter("SELECT * FROM schedules ORDER BY ID", con);
+                dt = new DataTable();
+                adpt.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+
+            }
+        }
+
+        private void showData()
+        {
+            con.Open();
+
+            adpt = new SqlDataAdapter("SELECT * FROM schedules ORDER BY ID", con);
+            dt = new DataTable();
+            adpt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void SearchData(string search)
+        {
+            con.Open();
+            string query = "SELECT * FROM schedules WHERE "+ searchingColumn +" like '%" + search + "%'";
+            adpt = new SqlDataAdapter(query, con);
+            dt = new DataTable();
+            adpt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void SearchBox_TextChanged(object sender, EventArgs e)
+        {
+            SearchData(SearchBox.Text);
+        }
+
+        private void SearchTypeComboBox_SelectedValueChanged(object sender, EventArgs e)
+        {
+            SearchLabel.Text = "Search by " + SearchTypeComboBox.Text;
+
+            SearchBox.Enabled = true;
+
+            string searchType = SearchTypeComboBox.Text;
+
+            switch (searchType)
+            {
+                case "Route":
+                    searchingColumn = "route";
+                    break;
+
+                case "Destination":
+                    searchingColumn = "ends_at";
+                    break;
+
+                case "Starting Point":
+                    searchingColumn = "start_from";
+                    break;
+
+                case "Departure Time":
+                    searchingColumn = "departure_time";
+                    break;
+
+                case "Type":
+                    searchingColumn = "type";
+                    break;
+            }
         }
     }
 }
